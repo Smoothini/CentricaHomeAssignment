08f89cbe4f1cb01d839ddc3e4defa0b3518ad9e6
Andrei Katona <katona.andrei@gmail.com>
2021-02-26 18:28:47 +0100

    Store Data contract

---
+++ DataLayer/Model/Store.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataLayer.Model
+{
+    [DataContract]
+    public class Store
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Info { get; set; }
+        [DataMember]
+        public District District { get; set; }
+    }
+}
