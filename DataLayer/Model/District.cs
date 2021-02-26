582c0d02fe8483af56764c7f9854169d7c9f07cc
Andrei Katona <katona.andrei@gmail.com>
2021-02-26 18:22:48 +0100

    District Contract Model

---
+++ DataLayer/Model/District.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using DataLayer.Model;
+
+namespace DataLayer.Model
+{
+    [DataContract]
+    public class District
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public SalesPerson PrimarySalesPerson { get; set; }
+        [DataMember]
+        public IEnumerable<SalesPerson> SecondarySalesPersons { get; set; }
+    }
+}
