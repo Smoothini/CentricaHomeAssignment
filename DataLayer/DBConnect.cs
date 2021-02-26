7de912736a99538fb353e1b56e3fa43d50d62b97
Andrei Katona <katona.andrei@gmail.com>
2021-02-26 18:22:20 +0100

    Database connector

---
+++ DataLayer/DBConnect.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.SqlClient;
+
+namespace DataLayer
+{
+    public class DBConnect
+    {
+        private string conString = "Data Source=DESKTOP-3DRJQ43\\SQLEXPRESS;Initial Catalog=CentricaDemo;Integrated Security=True";
+        private SqlConnection sqlConnection = null;
+
+        public DBConnect()
+        {
+            sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = conString;
+        }
+    }
+}
