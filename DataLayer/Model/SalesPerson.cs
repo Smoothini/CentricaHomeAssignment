1f934a1fd9441b34284ba11ea7fccee229df61d2
Andrei Katona <katona.andrei@gmail.com>
2021-02-26 18:25:32 +0100

    Salesperson Data Contract

---
+++ DataLayer/Model/SalesPerson.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace DataLayer.Model
+{
+    [DataContract]
+    public class SalesPerson
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public string Surname { get; set; }
+    }
+}
